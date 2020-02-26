c918bb8ae740dd23abc8f75afb5c0a0f6f3f88b8
Daniil Kistanov <daniilkistanov@csus.edu>
2020-02-25 22:43:58 -0800

    changed command line style

--- ctr.c
+++ ctr.c
@@ -2,13 +2,14 @@
  * @file ctr.c
  * @author Ted Krovetz, completed by Daniil Kistanov
  * @date 3 October 2018
- * @brief Encrypts or decrypts stdin to stdout
+ * @brief Encrypts or decrypts stdin to dest
  * assignment for a cryptography class
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define NONCE_LEN 12
 #define BLK_LEN 48
@@ -37,23 +38,36 @@ static void incrementBlock(unsigned char blk[BLK_LEN]) {
 
 
 int main(int argc, const char* argv[]) {
+	
+	if((argc != 5)) {
+		printf("follow this pattern please encryptor (e|d) password sourcefile destfile");
+		return EXIT_FAILURE;
+	}
+		
+	
 	size_t bytes_read;
 	unsigned char key[KEY_LEN] = {0};  /* Auto init to all zeros */
 	unsigned char nonce[NONCE_LEN];
 	unsigned char blk[BLK_LEN];
 	unsigned char counter[BLK_LEN] = {0};
 	unsigned char temp_blk[BLK_LEN];
+	double bytesCounter = 0;
+	clock_t start, end;
+	FILE *source = fopen(argv[3], "rb+");
+	FILE *dest = fopen(argv[4], "wb+");
+	
 
 	/* Setup key and nonce. Report command line errors */
-	if ((argc == 3) && (strlen(argv[1]) == 1) && (strlen(argv[2]) <= KEY_LEN) && ((argv[1][0] == 'e') || (argv[1][0] == 'd'))) {
+	if ((strlen(argv[1]) == 1) && (strlen(argv[2]) <= KEY_LEN) && ((argv[1][0] == 'e') || (argv[1][0] == 'd'))) {
+
 		if (argv[1][0] == 'e') {
 			// part where you read in random nonce
 			FILE *rand = fopen("/dev/urandom", "rb");
 			fread(nonce, 1, NONCE_LEN, rand);
 			fclose(rand);
-			fwrite(nonce, 1, NONCE_LEN, stdout);
+			fwrite(nonce, 1, NONCE_LEN, dest);
 		} else {
-			fread(nonce, 1, NONCE_LEN, stdin);
+			fread(nonce, 1, NONCE_LEN, source);
 		}
 		//put the nonce and key into blocks
 		memcpy(counter, nonce, NONCE_LEN);
@@ -62,10 +76,14 @@ int main(int argc, const char* argv[]) {
 		fprintf(stderr, "usage: %s d|e <pwd up to 48 chars>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+	start = clock();
 	
 	do {
-		bytes_read = fread(blk, 1, BLK_LEN, stdin);  
+		bytes_read = fread(blk, 1, BLK_LEN, source);  
 		if (bytes_read > 0) {
+			
+			//count block
+			bytesCounter += 1;
 			
 			// copy counter into temp value
 			memcpy(temp_blk, counter, BLK_LEN);
@@ -80,9 +98,16 @@ int main(int argc, const char* argv[]) {
 			incrementBlock(counter);
 			
 			// write the block to output
-			fwrite(temp_blk, 1, bytes_read, stdout);	
+			fwrite(temp_blk, 1, bytes_read, dest);	
 		}
 	} while (bytes_read == BLK_LEN);
+	
+	fclose(source);
+	
+	end = clock();
+	double difference = (double)(end - start)/CLOCKS_PER_SEC;
+	printf("Took %.2f seconds to process ~%.2f megabytes", difference, bytesCounter*48/1000000);
+	
 	
 	return EXIT_SUCCESS;
 }
