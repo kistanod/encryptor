7afc68167d264f69ef476b519ea0bb5bc75a6ba7
Daniil Kistanov <daniilkistanov@csus.edu>
2020-02-25 19:57:03 -0800

    bruh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ make
 mv encryptor /usr/local/bin
 
 echo attempted to install at /usr/bin/local
-echo try to run "encryptor e [password] < [name of the file] > [put your own name]
+echo try to run encryptor e [password] < [name of the file] > [put your own name]
